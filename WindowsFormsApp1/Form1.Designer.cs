39c46954a71d97756551b49c180c0783ee7739a9
Ansh Agarwal <52511028+awesomeansh10@users.noreply.github.com>
2022-10-17 20:01:32 +0100

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Mouse_control
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mouse_control));
+            this.clickorno = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clickorno
+            // 
+            resources.ApplyResources(this.clickorno, "clickorno");
+            this.clickorno.Name = "clickorno";
+            this.clickorno.Click += new System.EventHandler(this.clickorno_Click);
+            // 
+            // Mouse_control
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Controls.Add(this.clickorno);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Mouse_control";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label clickorno;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using WindowsInput;
+
+namespace WindowsFormsApp1
+{
+    public partial class Mouse_control : Form
+    {
+        double PreviousX = 0f;
+        double PreviousY = 0f;
+
+        double sens = 2500f;
+        double min = 5f;
+        double MovementX;
+        double MovementY;
+        KinectSensor kinectsensor = null;
+        BodyFrameReader bodyFrameReader = null;
+        Body[] bodies = null;
+
+        public Mouse_control()
+        {
+            InitializeComponent();
+            initialiseKinect();
+            
+        }
+        public void initialiseKinect()
+        {
+            kinectsensor = KinectSensor.GetDefault();
+            if(kinectsensor != null)
+            {
+                //turn on kinect
+                kinectsensor.Open();
+            }
+            bodyFrameReader = kinectsensor.BodyFrameSource.OpenReader();
+
+            if(bodyFrameReader != null)
+            {
+                bodyFrameReader.FrameArrived += Reader_FrameArrived;
+            }
+        }
+
+        private void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            bool dataReceived = false;
+            using(BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if(bodyFrame != null)
+                {
+                    if(bodies == null)
+                    {
+                        bodies = new Body[bodyFrame.BodyCount];
+                    }
+                }
+                bodyFrame.GetAndRefreshBodyData(bodies);
+                dataReceived = true;
+            }
+            if (dataReceived)
+            {
+                foreach (Body body in bodies)
+                {
+                    if (body.IsTracked)
+                    {
+                        IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
+                        Dictionary<JointType, Point> jointPoints = new Dictionary<JointType, Point>();
+
+                        Joint RightHand = joints[JointType.HandRight];
+
+                        //System.Diagnostics.Trace.Write(RightHand.Position.X);
+                        //System.Diagnostics.Trace.Write(RightHand.Position.Y);
+                        //System.Diagnostics.Trace.Write(RightHand.Position.Z);
+                        //System.Diagnostics.Trace.Write("\n");
+
+                        InputSimulator inputsim = new InputSimulator();
+
+                        MovementX = RightHand.Position.X - PreviousX;
+                        MovementY = RightHand.Position.Y - PreviousY;
+
+                        double newsens = RightHand.Position.Z * sens;
+                        MovementX *= sens;
+                        MovementY *= sens;
+
+                        MovementY = -MovementY;
+                        if(Math.Abs(MovementX) > min || Math.Abs(MovementY) > min){
+                            inputsim.Mouse.MoveMouseBy((int)MovementX, (int)MovementY);
+                            System.Diagnostics.Trace.WriteLine(MovementX);
+                            //System.Diagnostics.Trace.Write("moving", ToString(MovementX));
+                        }
+
+                        PreviousX = RightHand.Position.X;
+                        PreviousY = RightHand.Position.Y;
+                        
+                        if(body.HandRightState == HandState.Closed)
+                        {
+                            inputsim.Mouse.LeftButtonDown();
+                            clickorno.Text = "Click";
+                            clickorno.BackColor = Color.Green;
+                            
+                        }
+                        else
+                        {
+                            inputsim.Mouse.LeftButtonUp();
+                            clickorno.Text = "No Click";
+                            clickorno.BackColor = Color.Red;
+
+                        }
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clickorno_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
